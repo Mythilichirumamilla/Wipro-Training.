255a9cbf3e0106288fe3d044974c8b4bfde469f6
chiru <chiru@MYTHILI>
2026-01-08 20:55:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Please enter your name:"); // Prompt the user for input
+            string name = Console.ReadLine(); // Read user input and saving it to a variable of type string
+            Console.WriteLine("Hello, " + name + "!"); // Display a greeting message using the input
+
+
+        }
+    }
+}
